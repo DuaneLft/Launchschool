aad163c1d61dc08aaa4faa2c9abbc54163e23edf
Duane Leftwich <duane.lft@gmail.com>
2025-07-07 17:22:00 -0400

    Exercise for datatypes

---
+++ intro_to_py/data_types/names_pets.py
@@ -0,0 +1,16 @@
+names = (
+    'Asta',
+    'Butterscotch',
+    'Pudding',
+    'Neptune',
+    'Darwin',
+    'Sheba',
+)
+pets = {
+    'Asta': 'dog',
+    'Butterscotch': 'cat',
+    'Pudding': 'cat',
+    'Neptune': 'fish',
+    'Darwin': 'lizard',
+    'Sheba': 'dog',
+}
