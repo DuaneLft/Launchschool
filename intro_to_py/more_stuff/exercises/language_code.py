8303f4a3332ce06767edeae144df3047dc1f36b7
Duane Leftwich <duane.lft@gmail.com>
2025-07-20 23:42:35 -0400

    Last exercises

---
+++ intro_to_py/more_stuff/exercises/language_code.py
@@ -0,0 +1,24 @@
+def greet(iso):
+    match iso:
+        case 'en':
+            return 'Hi!'
+        case 'fr':
+            return 'Salut!'
+        case 'pt':
+            return 'Olá!'
+        case 'de':
+            return 'Hallo!'
+        case 'sv':
+            return 'Hej!'
+        case 'af':
+            return 'Haai!'
+        case _:
+            return "I don't know what language that is"
+
+print(greet('en'))         # Hi!
+print(greet('fr'))         # Salut!
+print(greet('pt'))         # Olá!
+print(greet('de'))         # Hallo!
+print(greet('sv'))         # Hej!
+print(greet('af'))
+print(greet('dd'))
---
+++ intro_to_py/more_stuff/exercises/vocabulary = [.py
@@ -0,0 +1,17 @@
+
+grocery_list = ['paprika', 'tofu', 'garlic', 'quinoa',
+                'carrots', 'broccoli', 'hummus']
+counter = 0
+while counter < len(grocery_list):
+    print(grocery_list.pop(0))
+    
+    
+
+
+    
+
+
+
+
+
+
